cbb3b46ec7e5ddb055b92f75ea8b829943fd7497
Frédéric Beauvois <boostup@funcarousel.home>
2017-11-06 21:31:54 +0100

    quick fix for 'selected route' in the navbar menu which was broken when upgrading to bootstrap v4.

--- src/ui/routenavitem/RouteNavItem.js
+++ src/ui/routenavitem/RouteNavItem.js
@@ -1,17 +1,33 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import { NavItem } from "react-bootstrap";
+import { Route, Link } from "react-router-dom";
+import "./RouteNavItem.css";
 
 export default props =>
   <Route
     path={props.href}
     exact
-    children={({ match, history }) =>
-      <NavItem
-        onClick={e => history.push(e.currentTarget.getAttribute("href"))}
-        {...props}
-        active={match ? true : false}
-      >
-        {props.children}
-      </NavItem>}
+    children={({ match, history }) => {
+      return (
+        <li className="nav-item">
+          <Link 
+            to={props.href}
+            className={`nav-link ${match ? 'active' : ''}`} 
+            onClick={e => history.push(e.currentTarget.getAttribute("href"))}
+            >{props.children}</Link>
+        </li>
+      )}}
   />;
+
+/**
+ * Works with bootstrap v3.  Keeping this for quick restoration in case needed.
+ */
+// import { NavItem } from "react-bootstrap";
+//Could not use NavItem from 'react-bootrap' because I prefer the stylesheet for bootstrap v4 than v3,
+//However, the 'react-bootrap' implement v3 of bootstrap...
+// <NavItem
+//   onClick={e => history.push(e.currentTarget.getAttribute("href"))}
+//   {...props}
+//   active={match ? true : false}
+// >
+//   {props.children}
+// </NavItem>
