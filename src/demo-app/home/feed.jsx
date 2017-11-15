6626b3273ce34daac5c4deabd7d19c5614fe2e3d
Frédéric Beauvois <boostup@funcarousel.home>
2017-11-15 11:34:13 +0100

    fixed the 'feed' section's looks

--- src/demo-app/home/feed.jsx
+++ src/demo-app/home/feed.jsx
@@ -10,8 +10,8 @@ export default (props) => {
             return (
                 <Feed key={item.id}>
                     <Feed.Event>
-                        <Feed.Label image={item.thumbnailUrl} />
-                        <Feed.Content content={`Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat eligendi dolorum sint est dicta, minima placeat dolor quis/`} />
+                        <Feed.Label content={<Image src={item.thumbnailUrl} />} />
+                        <Feed.Content content={item.title} />
                     </Feed.Event>
                 </Feed>                 
             )
