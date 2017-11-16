f81e2442474c185d68b72c6034eed5ee994043dc
Frédéric Beauvois <boostup@funcarousel.home>
2017-11-16 19:57:14 +0100

    added explanations about 'startLazyLoading'

--- src/demo-app/home/explanation.jsx
+++ src/demo-app/home/explanation.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 export default (props) => (
     <div>
-        <h2>Props origin</h2>
+        <h2>About the props above</h2>
         <ul>
+            <li>
+                <b>startLazyLoading:</b> this is for performance purposes (Critial Rendering Path)<b>false</b> on first screen render.  
+                As soon as the screen is scrolled, this component state prop will be equal to <b>true</b>,
+                which will trigger the lazy loading of the rest of the page.
+            </li>
             <li>
                 <b>fetchFeed:</b> function available through redux <b>connect()</b> mechanism.  
                 Have a look at the bottom of the <b>Home.jsx</b> component file</li>
