3d67cac56394dc5e73c9c2323b52da71dc7a4597
Frédéric Beauvois <boostup@funcarousel.home>
2017-11-29 07:28:30 +0100

    some legibility improvement

--- src/ui/home/feed.jsx
+++ src/ui/home/feed.jsx
@@ -7,18 +7,22 @@ export default (props) => {
         <div>
             <h2>Sample: feed items</h2>
             <Item.Group unstackable divided>
-            {props.feed.map(item => (
-                    <div className="item" key={item.childKey}>
-                        <Item.Image size='tiny' content={<ImageLoader src={item.image}/>} />
-                        <Item.Content>
-                            <Item.Header as='a'>{item.header}</Item.Header>
-                            <Item.Description>
-                                {item.description}   
-                            </Item.Description>
-                        </Item.Content>
-                    </div>                    
-            ))}
+            {props.feed.map(feedItem)}
             </Item.Group>
         </div>
     )
+}
+
+const feedItem = (item) => {
+    return (
+        <div className="item" key={item.childKey}>
+            <Item.Image size='tiny' content={<ImageLoader src={item.image}/>} />
+            <Item.Content>
+                <Item.Header as='a'>{item.header}</Item.Header>
+                <Item.Description>
+                    {item.description}   
+                </Item.Description>
+            </Item.Content>
+        </div>                    
+    )   
 }
